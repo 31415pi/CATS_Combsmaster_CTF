4a137e74235e04678fe43be58bb92b0f6a68185d
wekster <31289154+31415pi@users.noreply.github.com>
2020-10-31 01:27:22 -0700

    Fixed all bugs. Works
    Checked and kicks out a good win.

--- MD5_poper.c
+++ MD5_poper.c
@@ -10,20 +10,15 @@
 #include <assert.h>
 #include <openssl/md5.h>
  
-//#define SALT "d470d460"  // from line 57 of bot.sh
 void rand_str(char *, size_t);
 
-//char* itoa(int value, char* buffer, int base);
-
 int main(void) {
-	int n=1;
+	int n=0;
 	int m = 0;
-//	char result[] = "abcd232323232323232323232efg0badbeef23232323232";
-    char str[] = { [41] = '\1' }; 				// make the last character non-zero so we can test based on it later
+    char str[]= "lol0loll0l0l0l0l0l0l0l0l0l0l0l0l0";// make the last character non-zero so we can test based on it later
 	unsigned char result[MD5_DIGEST_LENGTH];
-	char salty[] = "d470d460"; 					// from line 57 of bot.sh
-	char buzzword[] = "0badbeef"; //string in the md5 that needs to be found
-
+	char salty[] = "d470d406"; 					// from line 57 of bot.sh
+	char buzzword[] = "0BADBEEF"; //string in the md5 that needs to be found
 	// start time stuff for seed
 	long            ms; // Milliseconds
     time_t          s;  // Seconds
@@ -32,73 +27,55 @@ int main(void) {
     s  = spec.tv_sec;
 	ms = spec.tv_nsec;
 	srand(ms); //seed with ms
-	int len = strlen(buzzword);
-
-
-
-	//debug stuff
-//	int debugquit = 40;			//limiting debug loop to 101 cycles to make sure random is gen quick enough 
+	int len = 33;
 	int found = 0;
-
-		//start while loop here:
-
-	while(found!= 1 ){
-		//debugquit--; // keep while loop to 10 max
-		//removed from while '&& (debugquit > 0)'
+	char testy[44]; // container for the conversion
+	testy[0] = '\0';
+	char buff[6]; //buffer while we convert
+	int i;
+	while(found != 1 ){
 		rand_str(str, sizeof str - 1); //get a random string
-		for(int v = 32; v < 41; v++){// appending salt
-			str[v] = salty[(v-32)];
+		strcat(str,salty);		
+		MD5(str, strlen(str),result); //hash salted string
+		char testy[44]; // container for the conversion
+		char buff[6]; //buffer while we convert
+		//		getting result into a working var
+		for(i = 0; i < MD5_DIGEST_LENGTH; i++) 
+		{		//take unsigned char and make to char
+			snprintf(buff, sizeof(buff), "%02X", (unsigned char)result[i]);
+			strcat(testy,buff);//write the char buffer to the working var
 		}
-		
-		MD5(str, strlen(str), result); //hash salted string
-
-//		printf("\n-%s : ",str);	//print the salted rando string and the result of hashing
-//		for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
-//			printf("%02x",result[i]);
-//		printf("\n");
-
-
-// character searching			
-		if (result[n] == buzzword[m]){ //match, keep looking
-			printf(" n%d,  m%d", n,m);
-			n++; // load next char in result
-			m++; //laod next char in buzz
-				while(result[n] == buzzword[m]){
-					printf(" n%d,  m%d", n,m);
-
+		for(i=1;i<=32;i++){ //parse working var for the extra prechars
+			testy[i-1]=testy[i];
+		}
+		testy[32]='\0';
+		// character searching		
+		while(n<33){ 
+				log[0]++;
+				n++; // load next char in result
+				m++; //laod next char in buzz
+				while(testy[n] == buzzword[m]){
+					log[m]++;
 					n++;
 					m++;
-					printf("+");
+					if(m ==  8){
+						printf("\n  ***FOUND***\n");
+						found = 1;
+						printf("%s : %s",str,testy);
+						printf("\n  ***FOUND***\n");
+						break;
+						} //if for final match
 				} //while for continued finding
-			printf("\nmatch");
-			
-			if(m == len ){
-				printf("\n  ***FOUND***\n");
-				found = 1;
-				printf("%s : ",str);
-				for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
-					printf("%02x",result[i]);
-				printf("\n  ***FOUND***\n");
-				break;
-				
-			} //if for final match
-			
-		}// char match end
-		printf('.');
-//		for(int i = 0; i < MD5_DIGEST_LENGTH; i++)
-//			printf("%02x",result[i]);
-//		printf(" %d,  %d", n,m);
-		n++; //goto next char in result because no match on cur char;
-		if (n==34)
-			printf('^');
-			n=0;
-		m=0; //start from first char in buzzword
+			}// char match end
+			n++; //goto next char in result because no match on cur char;
+			m=0; //start from first char in buzzword
+		}//END OF entire result parsing / while
+		n=0;
+		buff[1] = '\0';
+		testy[1] = '\0';
 	} // end of while loop
-
-			
 	return EXIT_SUCCESS;
 }
-
 void rand_str(char *dest, size_t length) {
     char charset[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     while (length-- > 0) {
